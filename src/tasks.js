35f3a834046f74ac50fcf145ab00cbed2c6288bd
Nadeesha Malshan <malshannandanayaka@gmail.com>
2026-09-24 17:49:09 +0530

    Delete tasks.js

--- src/tasks.js
+++
@@ -1,7 +0,0 @@
-// CampusEats task list
-const tasks = [
-    "Design the menu screen",
-    "Build the orders API",
-    "Add user login",
-];
-console.log(`CampusEats has ${tasks.length} open tasks`);
